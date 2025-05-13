eaece38e91e0dcd6e81a62b8a539c42b1a079cb8
Senhoreuu <luizsimonetti888@gmail.com>
2025-05-12 21:13:15 -0300

    up

--- Assets/Scripts/Inimigos.cs
+++ Assets/Scripts/Inimigos.cs
@@ -19,6 +19,8 @@ public class Inimigos : MonoBehaviour
 
     public bool inimigoAtirador;
     public bool inimigoAtivado;
+    public bool isBoss;
+    public bool isFinalBoss;
 
     // Start is called before the first frame update
     void Start()
